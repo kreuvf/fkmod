130c88dfa77e387ea268344e930574ffa0c9aae8
Fingolfin <fingolfin@warzone2100.de>
2017-08-04 22:00:30 +0200

    Add conversion script for weaponmodifier.py

---
+++ csv2json/weaponmodifier.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+
+import csv
+import json
+import re
+
+# Make utility functions available
+exec(compile(open("util.py", "r").read(), "util.py", 'exec'))
+
+#open needed files and copy their content into lists
+with open('../data/mp/stats/weaponmodifier.txt', 'r') as file:
+	reader = csv.reader(file, delimiter=',')
+	weapon = list(reader)
+
+# Build dictionary for JSON
+# Assignment of index to variable
+# 0 -> weapon effect
+# 1 -> building type
+# 2 -> modifier
+
+obj = dict()		
+for line in weapon:
+	if not line[0] in obj:
+		obj[line[0]] = {line[1] : int(line[2])}
+	att = obj[line[0]]
+	att[line[1]] = int(line[2])
+	obj[line[0]] = att
+	
+	
+with open('../jsondata/mp/stats/weaponmodifier.json', 'w') as f:
+	f.write(json.dumps(obj, sort_keys=True, indent=4)) 
