12d78cd6d87ad25615f47f6e1cf78992a29f4a48
Kreuvf <kreuvf@warzone2100.de>
2018-10-27 21:52:29 +0200

    * Convert sensor towers and hurricane AA sites

--- maps2fkmaps/maps2fkmaps-worker.sh
+++ maps2fkmaps/maps2fkmaps-worker.sh
@@ -8,11 +8,13 @@ sed -i -r \
 	-e 's/A0HardcreteMk1Wall/FKWall/g' \
 	-e 's/A0CyborgFactory/FKCyborgFactory/g' \
 	-e 's/A0ResearchFacility/FKResearchFacility/g' \
+	-e 's/AASite-QuadMg1/FKAASiteHurricane/g' \
 	-e 's/GuardTower6/FKHardpointScourgeMissile/g' \
 	-e 's/PillBox1/FKBunkerMG/g' \
 	-e 's/PillBox3/FKBunkerMG/g' \
 	-e 's/PillBox4/FKBunkerCannon/g' \
 	-e 's/PillBox5/FKPitHowitzerIncendiary/g' \
+	-e 's/Sys-SensoTower01/FKTowerArtillery/g' \
 	-e 's/WallTower01/FKHardpointMG/g' \
 	-e 's/WallTower02/FKHardpointCannon/g' \
 	-e 's/GuardTower1/FKHardpointMG/g' \
