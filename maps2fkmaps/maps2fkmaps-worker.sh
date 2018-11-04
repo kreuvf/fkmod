c8bd537ea522210fd352ebe47acf845b1a0cac46
Kreuvf <kreuvf@warzone2100.de>
2018-11-04 16:40:36 +0100

    * Add new replacements * Document inaccurate replacements

--- maps2fkmaps/maps2fkmaps-worker.sh
+++ maps2fkmaps/maps2fkmaps-worker.sh
@@ -7,9 +7,12 @@ sed -i -r \
 	-e 's/A0HardcreteMk1CWall/FKWallCorner/g' \
 	-e 's/A0HardcreteMk1Wall/FKWall/g' \
 	-e 's/A0CyborgFactory/FKCyborgFactory/g' \
+	-e 's/A0RepairCentre3/FKRepairFacility/g' \
 	-e 's/A0ResearchFacility/FKResearchFacility/g' \
 	-e 's/AASite-QuadMg1/FKAASiteHurricane/g' \
+	-e 's/GuardTower1/FKHardpointMG/g' \
 	-e 's/GuardTower6/FKHardpointScourgeMissile/g' \
+	-e 's/Pillbox-RotMG/FKBunkerMG/g' \
 	-e 's/PillBox1/FKBunkerMG/g' \
 	-e 's/PillBox3/FKBunkerMG/g' \
 	-e 's/PillBox4/FKBunkerCannon/g' \
@@ -17,5 +20,11 @@ sed -i -r \
 	-e 's/Sys-SensoTower01/FKTowerArtillery/g' \
 	-e 's/WallTower01/FKHardpointMG/g' \
 	-e 's/WallTower02/FKHardpointCannon/g' \
-	-e 's/GuardTower1/FKHardpointMG/g' \
+	-e 's/WallTower05/FKHardpointMG/g' \
 	"$1"
+
+# Inaccurate replacements necessary to prevent holes in walls:
+# 	GuardTower1: Heavy Machinegun Guard Tower
+# 	GuardTower6: Mini-Rocket Tower
+# 	Pillbox5: Flamer Bunker
+# 	WallTower05: Flamer Hardpoint
