fdf7754433e27e48e0a838a649160b7d7939d6d0
Fingolfin <fingolfin@warzone2100.de>
2019-03-16 21:59:26 +0100

    Add routine for mixed attack groups

--- jsondata/mp/multiplay/skirmish/fk_includes/units.js
+++ jsondata/mp/multiplay/skirmish/fk_includes/units.js
@@ -1,5 +1,4 @@
-var antiTankGroups = [];
-antiTankGroups.push(newGroup());
+var antiTankGroups = [newGroup()];
 var antiCyborgGroups = [newGroup()];
 var vtolGroups = [newGroup()];
 var mixedGroups = [newGroup()];
@@ -83,7 +82,7 @@ function unitControl() {
 				var unit = freeUnits[j];
 				if(isAntiTank(unit)) {
 					groupAdd(group, unit);
-					if(groupSize(group) >= tankGroupSize) break;
+					if(groupSize(group) >= tankGroupSize()) break;
 				}
 			}
 		} else {
@@ -95,9 +94,12 @@ function unitControl() {
 				units[0].order !== DORDER_SCOUT
 			) {
 				var target = findAntiTankTarget();
+				target = target.filter(function(droid) {
+					return droidCanReach(units[0], droid.x, droid.y);
+				});
 				if(target.length > 0) {
 					for(j = 0; j < units.length; j++) {
-					orderDroidObj(units[j], DORDER_ATTACK, target[0]);
+						orderDroidObj(units[j], DORDER_ATTACK, target[0]);
 					}
 				}
 			}
@@ -109,13 +111,13 @@ function unitControl() {
 		var group = antiCyborgGroups[i];
 		
 		// Check if we need to add new units
-		if(groupSize(group) < cyborgGroupSize()) {
+		if(groupSize(group) < tankGroupSize()) {
 			var freeUnits = enumGroup(ungrouped);
 			for(j = 0; j < freeUnits.length; j++) {
 				var unit = freeUnits[j];
 				if(isAntiCyborg(unit)) {
 					groupAdd(group, unit);
-					if(groupSize(group) >= cyborgGroupSize) break;
+					if(groupSize(group) >= tankGroupSize()) break;
 				}
 			}
 		} else {
@@ -127,9 +129,54 @@ function unitControl() {
 				units[0].order !== DORDER_SCOUT
 			) {
 				var target = findAntiCyborgTarget();
+				target = target.filter(function(droid) {
+					return droidCanReach(units[0], droid.x, droid.y);
+				});
 				if(target.length > 0) {
 					for(j = 0; j < units.length; j++) {
-					orderDroidObj(units[j], DORDER_ATTACK, target[0]);
+						orderDroidObj(units[j], DORDER_ATTACK, target[0]);
+					}
+				}
+			}
+		}
+	}
+	
+	// Mixed Groups
+	for(i = 0; i < mixedGroups.length; i++) {
+		var group = mixedGroups[i];
+		
+		// Check if we need to add new units
+		if(groupSize(group) < tankGroupSize()) {
+			var freeUnits = enumGroup(ungrouped);
+			var nrAntiTank = enumGroup(group).filter(isAntiTank).length;
+			var nrAntiCyborg = enumGroup(group).filter(isAntiCyborg).length;
+			for(j = 0; j < freeUnits.length; j++) {
+				var unit = freeUnits[j];
+				if(isAntiCyborg(unit) && nrAntiTank >= nrAntiCyborg) {
+					groupAdd(group, unit);
+					if(groupSize(group) >= tankGroupSize()) break;
+				}
+				if(isAntiTank(unit) && nrAntiCyborg >= nrAntiTank) {
+					groupAdd(group, unit);
+					if(groupSize(group) >= tankGroupSize()) break;
+				}
+			}
+		} else {
+			// Check if group needs orders
+			var units = enumGroup(group);
+			if(
+				units[0].order !== DORDER_ATTACK &&
+				units[0].order !== DORDER_RTR &&
+				units[0].order !== DORDER_SCOUT
+			) {
+				var target = findAntiCyborgTarget();
+				target.concat(findAntiTankTarget());
+				target = target.filter(function(droid) {
+					return droidCanReach(units[0], droid.x, droid.y);
+				});
+				if(target.length > 0) {
+					for(j = 0; j < units.length; j++) {
+						orderDroidObj(units[j], DORDER_ATTACK, target[0]);
 					}
 				}
 			}
